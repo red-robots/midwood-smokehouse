c551ebc48eb4f2e3c00419d2d1d778cd71b71073
lisadebona <annalisa.quiamco@gmail.com>
2019-06-19 22:14:23 -0600

    styling sitemap and 404 page

--- pages/page-sitemap.php
+++ pages/page-sitemap.php
@@ -3,20 +3,22 @@
  * Template Name: Sitemap
  */
 
-get_header(); ?>
-
-	<div id="primary" class="full-content-area wrapper default-template">
-		<main id="main" class="site-main clear" role="main">
-
+get_header(); 
+$banner = get_field('banner'); 
+$div_open = ($banner) ? '':'<div class="med-wrapper clear">'; 
+$div_close = ($banner) ? '':'</div>'; 
+?>
+<div id="primary" class="full-content-area clear single-page">
+	<main id="main" class="site-main wrapper clear" role="main">
+		<?php echo $div_open ?>
 			<?php
 			while ( have_posts() ) : the_post();
 				get_template_part( 'template-parts/content', 'page' );
 				get_template_part('template-parts/content','sitemap');
 			endwhile; // End of the loop.
 			?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
+		<?php echo $div_close ?>
+	</main><!-- #main -->
+</div><!-- #primary -->
 <?php
 get_footer();
