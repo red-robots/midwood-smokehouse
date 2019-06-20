1736ff0410e2fb15d69dcada46ea34f3304e0207
lisadebona <annalisa.quiamco@gmail.com>
2019-06-19 21:26:07 -0600

    Styling media page

--- template-parts/banner.php
+++ template-parts/banner.php
@@ -1,7 +1,7 @@
 <?php 
 $banner = get_field('banner');
 if($banner) { ?>
-<div class="banner">
+<div class="banner clear">
 	<img src="<?php echo $banner['url'] ?>" alt="<?php echo $banner['PDF_set_info_title()'] ?>" />
 </div>
 <?php } ?>
