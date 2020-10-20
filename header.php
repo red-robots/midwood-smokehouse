f5b57d7c27b1c43d6b64153ba663e41ef51a70c9
Austin Crane <austincrane@gmail.com>
2020-10-20 15:11:19 -0400

    add popup

--- header.php
+++ header.php
@@ -88,13 +88,13 @@ $btnLink = get_field('button_link', 'option');
 	</div>
 <?php } ?>
 
-<div style='display:none'>
+<!-- <div style='display:none'>
 	<div id='inline_content' class="ajax popup">
 		<a href="https://direct.chownow.com/order/1482/locations" target="_blank">
 			<img src="<?php bloginfo('template_url'); ?>/images/curbside.jpg">
 		</a>
 	</div>
-</div>
+</div> -->
 
 
 <div id="topDiv"></div>
